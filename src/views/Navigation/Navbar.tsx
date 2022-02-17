2199ed76b2da5ec7dec55b9f1c072aeaf96710f7
Jonas <boone.jonas@outlook.be>
2022-02-17 13:47:47 +0100

    build(package.json): added tailwind-config-viewer package and script

--- src/views/Navigation/Navbar.tsx
+++ src/views/Navigation/Navbar.tsx
@@ -1,32 +1,22 @@
-import React, { useState, createRef } from 'react';
-import styled from 'styled-components';
-import Burger from './Burger/Burger';
-import Menu from './Menu/Menu';
-import Header from '../../components/Header';
-import useOnClickOutside from '../../hooks/useOnClickOutside';
-
-const StyledNav = styled.nav`
-  background: #005f73;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
+import React from 'react';
 
 function Navbar() {
-  const [open, setOpen] = useState(false);
-  const node = createRef<HTMLDivElement>();
-  useOnClickOutside(node, () => setOpen(false));
-
   return (
-    <StyledNav>
-      <Header mx="auto" variant="primary">
-        EURISHOP
-      </Header>
-      <div ref={node}>
-        <Burger open={open} setOpen={setOpen} />
-        <Menu open={open} setOpen={setOpen} />
+    <header className="bg-gray-900">
+      <div>
+        <h2 className="text-center text-white">Eurishop</h2>
       </div>
-    </StyledNav>
+      <div>
+        <button type="button" className="text-gray-400">
+          <svg viewBox="0 0 24 24" className="fill-current h-6 w-6">
+            <path
+              fillRule="evenodd"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+            />
+          </svg>
+        </button>
+      </div>
+    </header>
   );
 }
 
