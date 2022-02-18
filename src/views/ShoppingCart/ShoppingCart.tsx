352fe2f1f87ae65a41871637763776a8e954b1da
Jonas <boone.jonas@outlook.be>
2022-02-18 11:57:57 +0100

    style(ShoppingCart): further changes to styling

--- src/views/ShoppingCart/ShoppingCart.tsx
+++ src/views/ShoppingCart/ShoppingCart.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import BasketItem from './BasketItem';
 import { CartItem, calculateTotalCartCost } from '../../domain/shoppingCart';
-import CardDetail from './CardDetail';
 
 interface ShoppingCartProps {
   cartItems: CartItem[];
@@ -19,117 +18,48 @@ function ShoppingCart({ cartItems, onUpdate, onClear }: ShoppingCartProps) {
   };
 
   return (
-    <div className="py-12">
-      <div className="max-w-md mx-auto bg-gray-100 shadow-lg rounded-lg md:max-w-5xl">
-        <div className="md:flex ">
-          <div className="w-full p-4 px-5 py-5">
-            <div className="md:grid md:grid-cols-3 gap-2 ">
-              <div className="col-span-2 p-5">
-                <h1 className="text-xl font-medium ">Shopping Cart</h1>
-                <div className="flex justify-between items-center mt-6 pt-6">
-                  <div className="flex items-center">
-                    {' '}
-                    <img src="https://i.imgur.com/EEguU02.jpg" width="60" className="rounded-full " />
-                    <div className="flex flex-col ml-3">
-                      {' '}
-                      <span className="md:text-md font-medium">Chicken momo</span>{' '}
-                      <span className="text-xs font-light text-gray-400">#41551</span>{' '}
+    <div className="max-w-md mx-auto shadow-lg rounded-lg md:max-w-5xl">
+      <div className="md:flex ">
+        <div className="w-full p-4 px-5 py-5">
+          <div className="md:grid md:grid-cols-3 gap-2 ">
+            <div className="col-span-2 p-5">
+              <h1 className="text-xl font-bold text-gray-800 ">Shopping Cart</h1>
+              <div className="flex flex-col mt-6 pt-6">
+                {cartItems.map((cartItem) => (
+                  <div className="flex justify-between my-2">
+                    <div className="flex items-center">
+                      <img src={cartItem.product.image} width="60" className="rounded-full" />
+                      <div className="flex flex-col ml-3">
+                        <span className="md:text-md font-semibold font-mono text-gray-900">
+                          {cartItem.product.title}
+                        </span>
+                        <span className="text-xs font-light text-gray-600">#{cartItem.product.sku}</span>
+                      </div>
+                    </div>
+
+                    <div className="flex items-center">
+                      <div className="pr-8 flex items-center ">
+                        <FontAwesomeIcon icon={faMinus} className="w-2 h-2 block" />
+                        <div className="focus:outline-none bg-gray-100 border h-6 w-8 rounded text-sm px-2 mx-2">
+                          {cartItem.quantity}
+                        </div>
+                        <FontAwesomeIcon icon={faPlus} className="w-2 h-2 block" />
+                      </div>
+                      <div className="pr-8 ">
+                        <span className="text-xs font-medium">$10.50</span>
+                      </div>
                     </div>
                   </div>
-                  <div className="flex justify-center items-center">
-                    <div className="pr-8 flex ">
-                      {' '}
-                      <span className="font-semibold">-</span>{' '}
-                      <input
-                        type="text"
-                        className="focus:outline-none bg-gray-100 border h-6 w-8 rounded text-sm px-2 mx-2"
-                        value="1"
-                      />{' '}
-                      <span className="font-semibold">+</span>{' '}
-                    </div>
-                    <div className="pr-8 ">
-                      {' '}
-                      <span className="text-xs font-medium">$10.50</span>{' '}
-                    </div>
-                    <div>
-                      {' '}
-                      <i className="fa fa-close text-xs font-medium" />{' '}
-                    </div>
-                  </div>
+                ))}
+              </div>
+              <div className="flex justify-between items-center mt-6 pt-6 border-t">
+                <div className="flex items-center">
+                  <i className="fa fa-arrow-left text-sm pr-2" />{' '}
+                  <span className="text-md font-medium text-blue-500">Continue Shopping</span>{' '}
                 </div>
-                <div className="flex justify-between items-center pt-6 mt-6 border-t">
-                  <div className="flex items-center">
-                    {' '}
-                    <img src="https://i.imgur.com/Uv2Yqzo.jpg" width="60" className="rounded-full " />
-                    <div className="flex flex-col ml-3 ">
-                      {' '}
-                      <span className="text-md font-medium w-auto">Spicy Mexican potatoes</span>{' '}
-                      <span className="text-xs font-light text-gray-400">#66999</span>{' '}
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center">
-                    <div className="pr-8 flex">
-                      {' '}
-                      <span className="font-semibold">-</span>{' '}
-                      <input
-                        type="text"
-                        className="focus:outline-none bg-gray-100 border h-6 w-8 rounded text-sm px-2 mx-2"
-                        value="1"
-                      />{' '}
-                      <span className="font-semibold">+</span>{' '}
-                    </div>
-                    <div className="pr-8">
-                      {' '}
-                      <span className="text-xs font-medium">$10.50</span>{' '}
-                    </div>
-                    <div>
-                      {' '}
-                      <i className="fa fa-close text-xs font-medium" />{' '}
-                    </div>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center mt-6 pt-6 border-t">
-                  <div className="flex items-center">
-                    {' '}
-                    <img src="https://i.imgur.com/xbTAITF.jpg" width="60" className="rounded-full " />
-                    <div className="flex flex-col ml-3 ">
-                      {' '}
-                      <span className="text-md font-medium">Breakfast</span>{' '}
-                      <span className="text-xs font-light text-gray-400">#86577</span>{' '}
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center">
-                    <div className="pr-8 flex">
-                      {' '}
-                      <span className="font-semibold">-</span>{' '}
-                      <input
-                        type="text"
-                        className="focus:outline-none bg-gray-100 border h-6 w-8 rounded text-sm px-2 mx-2"
-                        value="1"
-                      />{' '}
-                      <span className="font-semibold">+</span>{' '}
-                    </div>
-                    <div className="pr-8">
-                      {' '}
-                      <span className="text-xs font-medium">$10.50</span>{' '}
-                    </div>
-                    <div>
-                      {' '}
-                      <i className="fa fa-close text-xs font-medium" />{' '}
-                    </div>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center mt-6 pt-6 border-t">
-                  <div className="flex items-center">
-                    {' '}
-                    <i className="fa fa-arrow-left text-sm pr-2" />{' '}
-                    <span className="text-md font-medium text-blue-500">Continue Shopping</span>{' '}
-                  </div>
-                  <div className="flex justify-center items-end">
-                    {' '}
-                    <span className="text-sm font-medium text-gray-400 mr-1">Subtotal:</span>{' '}
-                    <span className="text-lg font-bold text-gray-800 "> $24.90</span>{' '}
-                  </div>
+                <div className="flex justify-center items-end">
+                  <span className="text-sm font-medium text-gray-400 mr-1">Subtotal:</span>{' '}
+                  <span className="text-lg font-bold text-gray-800 "> $24.90</span>{' '}
                 </div>
               </div>
             </div>
