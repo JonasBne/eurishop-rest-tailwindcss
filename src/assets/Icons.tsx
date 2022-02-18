af77b462df0a3bfe83397a844281a2ae70a2b2f3
Jonas <boone.jonas@outlook.be>
2022-02-18 09:31:06 +0100

    build: added hero icons

--- src/assets/Icons.tsx
+++ src/assets/Icons.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 
-export function Burger() {
-  <svg viewBox="0 0 24 24" className="fill-current h-6 w-6">
-    <path
-      fillRule="evenodd"
-      d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-    />
-  </svg>;
+export function BurgerIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path d="M12 14l9-5-9-5-9 5 9 5z" />
+      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+      />
+    </svg>
+  );
 }
 
-export function Cross() {
-  <svg viewBox="0 0 24 24" className="fill-current h-6 w-6">
-    <path
-      fillRule="evenodd"
-      d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-    />
-  </svg>;
+export function CrossIcon() {
+  return (
+    <svg viewBox="0 0 24 24" className="fill-current h-6 w-6">
+      <path
+        fillRule="evenodd"
+        d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+      />
+    </svg>
+  );
+}
+
+export function ShoppingCartIcon() {
+  return (
+    <svg className="flex-1 h-6 w-6 fill-current" viewBox="0 0 24 24">
+      <path d="M17,18C15.89,18 15,18.89 15,20A2,2 0 0,0 17,22A2,2 0 0,0 19,20C19,18.89 18.1,18 17,18M1,2V4H3L6.6,11.59L5.24,14.04C5.09,14.32 5,14.65 5,15A2,2 0 0,0 7,17H19V15H7.42A0.25,0.25 0 0,1 7.17,14.75C7.17,14.7 7.18,14.66 7.2,14.63L8.1,13H15.55C16.3,13 16.96,12.58 17.3,11.97L20.88,5.5C20.95,5.34 21,5.17 21,5A1,1 0 0,0 20,4H5.21L4.27,2M7,18C5.89,18 5,18.89 5,20A2,2 0 0,0 7,22A2,2 0 0,0 9,20C9,18.89 8.1,18 7,18Z" />
+    </svg>
+  );
 }
--- src/views/Navigation/Navbar.tsx
+++ src/views/Navigation/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { BurgerIcon, CrossIcon, ShoppingCartIcon } from '../../assets/Icons';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,26 +23,12 @@ function Navbar() {
               className="block text-gray-400 focus:text-white outline-none hover:text-white"
               onClick={handleClick}
             >
-              <svg viewBox="0 0 24 24" className="fill-current h-6 w-6">
-                {isOpen && (
-                  <path
-                    fillRule="evenodd"
-                    d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                  />
-                )}
-                {!isOpen && (
-                  <path
-                    fillRule="evenodd"
-                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                  />
-                )}
-              </svg>
+              {isOpen && <CrossIcon />}
+              {!isOpen && <BurgerIcon />}
             </button>
           </div>
           <button type="button" className="relative flex text-gray-400">
-            <svg className="flex-1 h-6 w-6 fill-current" viewBox="0 0 24 24">
-              <path d="M17,18C15.89,18 15,18.89 15,20A2,2 0 0,0 17,22A2,2 0 0,0 19,20C19,18.89 18.1,18 17,18M1,2V4H3L6.6,11.59L5.24,14.04C5.09,14.32 5,14.65 5,15A2,2 0 0,0 7,17H19V15H7.42A0.25,0.25 0 0,1 7.17,14.75C7.17,14.7 7.18,14.66 7.2,14.63L8.1,13H15.55C16.3,13 16.96,12.58 17.3,11.97L20.88,5.5C20.95,5.34 21,5.17 21,5A1,1 0 0,0 20,4H5.21L4.27,2M7,18C5.89,18 5,18.89 5,20A2,2 0 0,0 7,22A2,2 0 0,0 9,20C9,18.89 8.1,18 7,18Z" />
-            </svg>
+            <ShoppingCartIcon />
             <span className="absolute right-0 top-0 rounded-full bg-blue-600 w-3 h-3 top right p-0 m-0 text-white font-mono text-xs leading-tight text-center">
               5
             </span>
