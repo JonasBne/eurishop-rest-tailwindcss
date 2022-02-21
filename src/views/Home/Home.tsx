19c790fb5ae2c3ebd25c56a147374d2501aab30c
Jonas <boone.jonas@outlook.be>
2022-02-21 14:55:10 +0100

    refactor(Home): Home component no longer renders shopping cart (this'll be moved to separate route)

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,9 @@
 /* eslint-disable jsx-a11y/aria-role */
 import React, { useState, useEffect } from 'react';
-import { useAtom } from 'jotai';
 import { useGetProducts } from '../../api/productsApi';
 import ErrorModal from '../../components/ErrorModal/ErrorModal';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import ProductCard from './ProductCard';
-// eslint-disable-next-line import/no-cycle
-import ShoppingCart from '../ShoppingCart/ShoppingCart';
 import {
   useGetBasket,
   useMutationBasketPost,
@@ -16,13 +13,10 @@ import {
 } from '../../api/basketApi';
 import toasts from '../../components/toasts';
 import Button from '../../components/Button';
-// eslint-disable-next-line import/no-cycle
-// import { cartIsOpenAtom } from '../../App';
 
 function Home() {
   const { succesToast, failToast } = toasts();
   const [page, setPage] = useState<number>(0);
-  // const [cartIsOpen] = useAtom(cartIsOpenAtom);
   const { isLoading, error, products } = useGetProducts(page);
   const { mutate: postItemToBasket, error: postBasketError, data: postedData } = useMutationBasketPost();
   const { mutate: patch, error: patchBasketError, data: patchedData } = useMutationBasketPatch();
@@ -98,21 +92,16 @@ function Home() {
       {!isLoading && error && <ErrorModal name={error.name} message={error.message} />}
       {products && (
         <>
-          {!cartIsOpen && (
-            <>
-              <div className="flex flex-wrap justify-center sm:justify-evenly">
-                {products.map((product) => (
-                  <ProductCard key={product.id} product={product} onBuy={handleBuy} />
-                ))}
-              </div>
-              <div className="flex justify-center my-6">
-                <Button variant="primary" className="py-2 px-4" onClick={handleLoadMoreData}>
-                  Load more...
-                </Button>
-              </div>
-            </>
-          )}
-          {cartIsOpen && <ShoppingCart cartItems={cartItems} onUpdate={handleUpdate} onClear={handleClear} />}
+          <div className="flex flex-wrap justify-center sm:justify-evenly">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} onBuy={handleBuy} />
+            ))}
+          </div>
+          <div className="flex justify-center my-6">
+            <Button variant="primary" className="py-2 px-4" onClick={handleLoadMoreData}>
+              Load more...
+            </Button>
+          </div>
         </>
       )}
     </>
