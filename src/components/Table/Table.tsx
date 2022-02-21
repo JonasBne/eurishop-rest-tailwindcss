89c4d2ef967e371c374a49d81bda40573d823e21
Jonas <boone.jonas@outlook.be>
2022-02-21 12:48:42 +0100

    refactor(Table): added to-do's

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,15 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/require-default-props */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/aria-role */
 /* eslint-disable react/no-array-index-key */
-/* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react/require-default-props */
 import React from 'react';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import FaIcon from '../../assets/FaIcon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SortableTableHead from './SortableTableHead';
 import Button from '../Button';
+import noop from '../../utils/noop';
 
 interface Column {
   name: string;
@@ -28,71 +27,85 @@ interface TableProps {
   columns: Column[];
 }
 
+/*
+
+TODO
+
+- fix key issue
+- how to make height of th fit the right columns content?
+
+*/
+
 function Table({
   data,
   columns,
   sortExpression,
   setSortExpression,
-  onRowClick,
-  onActionClick,
+  onRowClick = noop,
+  onActionClick = noop,
   onLoadData,
 }: TableProps) {
   return (
-    <table className="grid grid-cols-2 my-8 lg:hidden">
-      {data.map((item) => (
-        <>
-          <thead key={`tablehead${item.id}`} className="bg-blue-800 my-4">
-            <tr className="flex flex-col my-2">
-              {columns.map((col, index) => (
-                <SortableTableHead
-                  className="py-2 px-3 text-sm"
-                  title={col.label}
-                  index={index}
-                  name={col.name}
-                  sortable={col.sortable}
-                  sortExpression={sortExpression}
-                  setSortExpression={setSortExpression}
-                  key={col.id}
-                />
-              ))}
-            </tr>
-          </thead>
-          <tbody role="tablebody" key={`tablebody${item.id}`}>
-            <tr key={`tablerow${item.id}`} role="row" className="flex flex-col">
-              {Object.keys(data[0]).map((title: any, index) => (
-                <td
-                  role="cell"
-                  key={`tabledata${item.id}`}
-                  className="p-1 m-1 text-left"
-                  onClick={() => {
-                    if (onRowClick !== undefined) {
-                      onRowClick(item.id);
-                    }
-                  }}
-                >
-                  {item[title]}
+    <>
+      <table className="table-fixed break-words grid grid-cols-2 my-8 mx-4 border-collapse shadow-2xl lg:hidden">
+        {data.map((item, index) => (
+          <>
+            <thead key={`tablehead${item.id}${index}`} className="bg-blue-600 w-fit my-4">
+              <tr className="flex flex-col my-2">
+                {columns.map((col, colIndex) => (
+                  <SortableTableHead
+                    className="py-2 px-3 text-xs"
+                    title={col.label}
+                    index={colIndex}
+                    name={col.name}
+                    sortable={col.sortable}
+                    sortExpression={sortExpression}
+                    setSortExpression={setSortExpression}
+                    key={`column${col.id}`}
+                  />
+                ))}
+              </tr>
+            </thead>
+            <tbody role="tablebody" key={`tablebody${item.id}`} className="my-4 text-xs ">
+              <tr key={`tablerow${item.id}`} role="row" className="flex flex-col my-4">
+                {Object.keys(data[0]).map((title: any, rowIndex) => (
+                  <td
+                    role="cell"
+                    key={`tabledata${rowIndex}${item.id}`}
+                    className="py-2 text-left"
+                    onClick={() => {
+                      if (onRowClick !== undefined) {
+                        onRowClick(item.id);
+                      }
+                    }}
+                  >
+                    {item[title]}
+                  </td>
+                ))}
+                <td role="cell" className="text-left py-2" key={`data${item.id}`}>
+                  <FontAwesomeIcon
+                    role="img"
+                    aria-label="trash-bin"
+                    className="w-8 h-8 text-red-600"
+                    icon={faTrash}
+                    onClick={() => {
+                      if (onActionClick !== undefined) {
+                        onActionClick(item.id);
+                      }
+                    }}
+                  />
                 </td>
-              ))}
-              <td role="cell" className="text-left">
-                <FaIcon
-                  role="img"
-                  aria-label="trash-bin"
-                  icon={faTrash}
-                  px="0.25rem"
-                  color="red"
-                  mx="1rem"
-                  onClick={() => {
-                    if (onActionClick !== undefined) {
-                      onActionClick(item.id);
-                    }
-                  }}
-                />
-              </td>
-            </tr>
-          </tbody>
-        </>
-      ))}
-    </table>
+              </tr>
+            </tbody>
+          </>
+        ))}
+      </table>
+      <div className="flex justify-center items-center">
+        <Button type="button" variant="secondary" onClick={onLoadData}>
+          LOAD MORE...
+        </Button>
+      </div>
+    </>
   );
 }
 
