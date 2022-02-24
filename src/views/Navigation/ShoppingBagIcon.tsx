2bb1ca6d1def864c3a10ac58921647ff841273d3
Jonas <boone.jonas@outlook.be>
2022-02-24 10:23:06 +0100

    ci: added rule to disable react/require-default-props

--- src/views/Navigation/ShoppingBagIcon.tsx
+++ src/views/Navigation/ShoppingBagIcon.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/require-default-props */
 import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
