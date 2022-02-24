00fd001484aaa2aed9cde69046556c98f85342d5
Jonas <boone.jonas@outlook.be>
2022-02-24 10:53:18 +0100

    refactor(LoadingSpinner): rewrote to Tailwind

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,25 +1,32 @@
 /* eslint-disable jsx-a11y/aria-role */
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
-import { space, SpaceProps } from 'styled-system';
-
-const spin = keyframes`
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-`;
-
-const Loader = styled.div<SpaceProps>`
-  ${space};
-  border: 16px solid #f3f3f3;
-  border-top: 16px solid #3498db;
-  border-radius: 50%;
-  width: 60px;
-  height: 60px;
-  animation: ${spin} 2s linear infinite;
-`;
 
 function LoadingSpinner() {
-  return <Loader mx="auto" mt="3rem" role="loading" aria-busy="true" />;
+  return (
+    <div
+      className="absolute z-50 w-full h-full overflow-hidden flex justify-center items-center"
+      role="loading"
+      aria-busy="true"
+    >
+      <div className="h-14 w-14 animate-pulse bg-blue-800 rounded-full flex justify-center items-center">
+        <svg
+          className="w-12 h-12 text-white animate-spin"
+          stroke="currentColor"
+          fill="none"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+          />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+        </svg>
+      </div>
+    </div>
+  );
 }
 
 export default LoadingSpinner;
