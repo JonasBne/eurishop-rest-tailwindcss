6a5c3b00d9e778f787340f101b7d261702bc9366
Jonas <boone.jonas@outlook.be>
2022-02-24 10:28:32 +0100

    refactor(ErrorModal): removed styled components

--- src/components/ErrorModal/Backdrop.tsx
+++ src/components/ErrorModal/Backdrop.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Div = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: 20;
-  background-color: rgba(0, 0, 0, 0.75);
-`;
 
 function Backdrop() {
-  return <Div />;
+  return <div className="fixed top-0 left-0 w-full h-screen z-20 bg-gray-900" />;
 }
 
 export default Backdrop;
--- src/views/Navigation/ShoppingBagIcon.tsx
+++ src/views/Navigation/ShoppingBagIcon.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
+/* eslint-disable react/require-default-props */
 import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
 import { useGetBasket } from '../../api/basketApi';
 
 interface ShoppingBagIconProps {
